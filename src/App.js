9d287c144c83b5df28ed9857c49b95ae5e0945a5
Svetlana Shinkar <svetanek@gmail.com>
2021-05-10 22:21:21 -0400

    fetched countries and displayed in dropdown menu with a choice of country set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+import {
+  MenuItem,
+  FormControl,
+  Select
+
+} from "@material-ui/core";
+
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      try {
+        const {data} = await axios.get("https://disease.sh/v3/covid-19/countries")
+        const countries = data.map((country) => ({
+              name: country.country,
+              value: country.countryInfo.iso2,
+            }));
+          setCountries(countries);
+      } catch (error) {
+        console.error(error);
+      }
+
+      // await fetch("https://disease.sh/v3/covid-19/countries")
+        // .then((response) => response.json())
+        // .then((data) => {
+        //   const countries = data.map((country) => ({
+        //     name: country.country,
+        //     value: country.countryInfo.iso2,
+
+
+          // const sortedData = sortData(data);
+          // setTableData(sortedData);
+          // setMapCountries(data);
+
+        // });
+    };
+    getCountriesData();
+  }, [])
+
+  const onCountryChange =  (e) => {
+    //value  is equal to index
+const countryCode = e.target.value;
+setCountry(countryCode)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+      <h1>Covid-19 Tracker</h1>
+      <FormControl className="app__dropdown">
+        <Select value={country} variant="outlined" onChange={onCountryChange}>
+         <MenuItem value="worldwide">Worldwide</MenuItem>
+          {countries.map((country, i) => (
+            <MenuItem value={country.value} key={i}>{country.name}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      </div>
     </div>
   );
 }
