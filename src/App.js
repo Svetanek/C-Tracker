e400190fc6bfa268c2df9ce7d1d5d8767df57347
Svetlana Shinkar <svetanek@gmail.com>
2021-05-11 00:04:51 -0400

    fetched data for a chosen country

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import axios from 'axios'
 import {
   MenuItem,
   FormControl,
-  Select
+  Select, Card, CardContent
 
 } from "@material-ui/core";
 import InfoBox from "./InfoBox";
+import Map from './Map'
 import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -43,15 +45,37 @@ function App() {
     getCountriesData();
   }, [])
 
-  const onCountryChange =  (e) => {
+  const onCountryChange = async (e) => {
     //value  is equal to index
 const countryCode = e.target.value;
 setCountry(countryCode)
+const url =
+countryCode === "worldwide"
+  ? "https://disease.sh/v3/covid-19/all"
+  : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
 
+  const {data} = await axios.get(url)
+  setCountry(countryCode);
+  setCountryInfo(data);
+
+  // await fetch(url)
+  // .then((response) => response.json())
+  // .then((data) => {
+  //   setCountry(countryCode);
+    // setCountryInfo(data);
+    // setLoading(false);
+    // // console.log([data.countryInfo.lat, data.countryInfo.long]);
+    // countryCode === "worldwide"
+    //   ? setMapCenter([34.80746, -40.4796])
+    //   : setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+    // setZoom(4);
+  // });
   }
+  console.log(countryInfo);
 
   return (
     <div className="app">
+      <div className="app__left">
       <div className="app__header">
       <h1>Covid-19 Tracker</h1>
       <FormControl className="app__dropdown">
@@ -67,12 +91,16 @@ setCountry(countryCode)
         <InfoBox title="Coronovirus Cases" cases="123" total="2000"/>
         <InfoBox title="Recovered" cases="123"  total="2000"/>
         <InfoBox title="Deaths" cases="0"  total="0"/>
-
       </div>
+      <Map/>
+      </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+        </CardContent>
+         </Card>
 
 
-
-      {/*Map*/}
     </div>
   );
 }
